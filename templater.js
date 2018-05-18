a6ce83bb50161d212324464a742ba2fcfbd54f8c
Yaroslav/Mazhar <ym.frontend@gmail.com>
2018-05-18 20:28:24 +0300

    stage 2

--- templater.js
+++ templater.js
@@ -1,13 +1,35 @@
-const Templater = {
-  run: function () {
-    const customBootstrapButton = document.querySelectorAll('bootstrap_button');
-    const arrCustomBootstrapButton = Array.from(customBootstrapButton);
+function HtmlTagsBuilder() {
 
-    for (let i = 0; i < arrCustomBootstrapButton.length; i++) {
-      const defaultBootstrapSubmitButtonText = "Some Text";
+  this.tagsObj = {};
 
-      arrCustomBootstrapButton[i].outerHTML = '<button class="btn btn-default">' + defaultBootstrapSubmitButtonText + '</button>';
+}
 
+
+HtmlTagsBuilder.prototype.run = function () {
+
+  for(let customTag in this.tagsObj ){
+
+    const customTags = Array.from(document.querySelectorAll(customTag));
+
+    if(customTags.length){
+      customTags[0].outerHTML = this.tagsObj[customTag];
+    }else {
+      console.log(customTag + " " +  'no customTag in DOM');
     }
   }
+
 };
+
+HtmlTagsBuilder.prototype.addTag = function (customTag, template) {
+
+  this.tagsObj[customTag] = template;
+
+};
+
+
+const Templater = new HtmlTagsBuilder();
+
+// Templater.addTag('bootstrap_button', '<button class="btn btn-default" type="submit">' + "Some text" + "</button>");
+// Templater.addTag('bootstrap_link', '<a class="btn btn-default" href="#" role="button">' + "Some Another Text" + "</a>");
+// Templater.run();
+
