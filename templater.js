ad468547adac2be359a9d38861c2ff3c456c828e
Yaroslav Mazhar <ymazhar@justcoded.com>
2018-06-06 13:17:11 +0300

    fix code organisation

--- templater.js
+++ templater.js
@@ -1,55 +1,53 @@
 ;(function ($) {
 
-  const pluginName = "templater",
-    defaults = {
-      tags: {}
-    };
+  const pluginName = "templater";
 
-  function Plugin(element, options) {
-    this.element = $(element);
-    this.config = $.extend({}, defaults, options);
+  class Plugin {
+    constructor(element, options) {
+      this.element = $(element);
+      this.config = $.extend({}, $.fn.templater.defaults, options);
 
-    this.init();
-  }
-
-  Plugin.prototype.init = function () {
-    const regexp = /{{(.*?)}}/g;
-
-    function _render(template, element) {
-
+      this.init();
+    }
+    render(template, element){
+      const regexp = /{{(.*?)}}/g;
       const $element = $(element);
 
-      function precedeTemplate(match, attr) {
-        if (attr === 'html') {
-          if ($element.html() === "") {
-            return $element.html('Some Text')[0].innerHTML
-          }
+      return template.replace(regexp, function (match, attr) {
+        if (attr === "html") {
           return $element.html();
-        } else {
-          return $element.attr(attr);
         }
-      }
-
-      return template.replace(regexp, precedeTemplate);
-    }
-
-    for (let tag in this.config.tags) {
-      $(this.element.find(tag)).each((i, element) => {
-
-        const template = _render(this.config.tags[tag], element);
-
-        this.element.find(tag).replaceWith(template);
+        return $element.attr(attr);
       });
     }
-  };
+
+    init() {
+
+      for (let tag in this.config.tags) {
+
+
+
+        $(this.element.find(tag)).each((i, element) => {
+          const template = this.render(this.config.tags[tag], element);
+
+         this.element.find(tag).replaceWith(template);
+        });
+      }
+    }
+  }
+
 
   $.fn.templater = function (options) {
     return this.each(function () {
-      if (!$.data(this, "plugin_" + pluginName)) {
-        $.data(this, "plugin_" + pluginName,
+      if (!$.data(this, `plugin_${pluginName}`)) {
+        $.data(this, `plugin_${pluginName}`,
           new Plugin(this, options));
       }
     });
+  };
+
+  $.fn.templater.defaults = {
+    tags: {}
   }
 
 })(jQuery);
